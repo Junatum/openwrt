b82fcf9d0d6e19f21a592f2cc390fad60a467f20
Beomjun Kang <kals323@gmail.com>
2024-11-28 12:01:09 +0000

    NET-11 linux 부팅 성공

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -508,6 +508,24 @@ define Device/h3c_magic-nx30-pro
 endef
 TARGET_DEVICES += h3c_magic-nx30-pro
 
+define Device/iptime-ax3ksm
+  UIMAGE_NAME := ax3ksm
+  BOARD_NAME := mt7981-AX3000
+  DEVICE_VENDOR := ipTIME
+  DEVICE_MODEL := AX3000SM
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_DTS := mt7981b-iptime-ax3ksm
+  IMAGE_SIZE := 114688k
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata | iptime-crc32 ax3ksm
+
+  # DEVICE_PACKAGES := kmod-usb3 kmod-mt7981-firmware mt7981-wo-firmware
+  # UBINIZE_OPTS := -E 5
+  # KERNEL_IN_UBI := 1
+endef
+TARGET_DEVICES += iptime-ax3ksm
+
 define Device/jcg_q30-pro
   DEVICE_VENDOR := JCG
   DEVICE_MODEL := Q30 PRO
