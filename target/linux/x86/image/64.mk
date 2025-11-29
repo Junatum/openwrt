b2f4012356db5de561213c7c96ce403599d1dcfc
Beomjun Kang <kals323@gmail.com>
2025-11-29 22:16:42 +0900

    x86: 64: add support for Smart Safe Hub Mark 3
    Signed-off-by: Beomjun Kang <kals323@gmail.com>

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -8,3 +8,14 @@ define Device/generic
   GRUB2_VARIANT := generic
 endef
 TARGET_DEVICES += generic
+
+define Device/smart-safe-hub_mark-3
+  DEVICE_VENDOR := Smart Safe Hub
+  DEVICE_MODEL := Mark 3
+  DEVICE_PACKAGES += \
+	kmod-dwmac-intel kmod-e1000e kmod-e1000 kmod-fs-vfat kmod-igb kmod-igc \
+	kmod-ixgbe kmod-r8169 kmod-drm-i915 kmod-iwlwifi kmod-rtw89-8852be \
+	openssh-server mitmproxy
+  GRUB2_VARIANT := generic
+endef
+TARGET_DEVICES += smart-safe-hub_mark-3
